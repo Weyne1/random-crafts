fd426347e8bdc9a85707b5ae4f39283917ead24e
Weyne1 <weyne931@gmail.com>
2026-05-05 20:26:18 +0300

    feat: overhaul tier calculation logic and bump to 1.5.0
    - Обновлён номер версии на 1.5.0
    - CoreItem изменён на class вместо record для более простой
      и быстрой установки тиров setTier в RecipeGraph
    
    Обновлен расчет тиров:
    - Тир -1 больше не существует и не используется (минимальный 0)
    - Добавлены новые фильтры для расчета тиров на основе тегов
      предмета/блока
    - Добавлен фильтр для расчета тиров на основе редкости предмета
    - Фильтр ANCHOR_RULES был отредактирован для работы
      с новыми фильтрами
    
    Обновлены группы для настроек игровых правил:
    - Обновлена группа TOOLS_AND_ARMOR: +FlintAndSteelItem, +ShearsItem
    - Обновлена группа FUNCTIONAL_BLOCKS: +CrafterBlock

--- common/src/main/java/net/weyne1/randomcrafts/core/tier/TierCalculator.java
+++ common/src/main/java/net/weyne1/randomcrafts/core/tier/TierCalculator.java
@@ -1,7 +1,9 @@
 package net.weyne1.randomcrafts.core.tier;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -11,17 +13,15 @@ import net.weyne1.randomcrafts.core.recipe.VanillaRecipeData;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static net.weyne1.randomcrafts.RandomCrafts.LOGGER;
 
 public class TierCalculator {
 
     public static Map<Item, Integer> calculateTier(List<VanillaRecipeData> vanillaRecipes) {
-
         Map<Item, Set<Item>> graph = buildGraph(vanillaRecipes);
-
         Set<Item> locked = new HashSet<>();
-
         Set<Item> allItems = new HashSet<>();
 
         for (VanillaRecipeData vr : vanillaRecipes) {
@@ -37,62 +37,10 @@ public class TierCalculator {
         allItems.addAll(graphItems);
 
         Map<Item, Integer> tiers = initializeBaseTiers(allItems, vanillaRecipes, locked);
-
         propagateTiers(graph, tiers, locked);
-
         //logGroupedTiers(tiers);
-
         return tiers;
     }
-
-    private record AnchorRule(String keyword, int tier) {}
-
-    private static final List<AnchorRule> ANCHOR_RULES = List.of(
-            new AnchorRule("netherite", 15),
-            new AnchorRule("banner_pattern", 15),
-            new AnchorRule("smithing_template", 15),
-
-            new AnchorRule("ender", 14),
-            new AnchorRule("elytra", 14),
-            new AnchorRule("dragon", 14),
-            new AnchorRule("end_", 14),
-            new AnchorRule("chorus", 14),
-            new AnchorRule("shulker", 14),
-            new AnchorRule("purpur", 14),
-
-            new AnchorRule("heavy_core", 13),
-            new AnchorRule("enchanted_golden_apple", 13),
-            new AnchorRule("mace", 13),
-            new AnchorRule("creeper_head", 13),
-            new AnchorRule("diamond", 13),
-            new AnchorRule("wither", 13),
-            new AnchorRule("beacon", 13),
-            new AnchorRule("conduit", 13),
-
-            new AnchorRule("blaze", 12),
-            new AnchorRule("ghast", 12),
-            new AnchorRule("magma", 12),
-            new AnchorRule("nether", 12),
-            new AnchorRule("quartz", 12),
-            new AnchorRule("soul", 12),
-            new AnchorRule("crimson", 12),
-            new AnchorRule("warped", 12),
-            new AnchorRule("blackstone", 12),
-            new AnchorRule("basalt", 12),
-
-            new AnchorRule("torchflower", 11),
-            new AnchorRule("pither", 11),
-            new AnchorRule("heart_of_the_sea", 10),
-            new AnchorRule("breeze", 10),
-            new AnchorRule("prismarine", 10),
-            new AnchorRule("gold", 10),
-            new AnchorRule("iron", 9),
-            new AnchorRule("copper", 7),
-            new AnchorRule("disc", 6),
-
-            new AnchorRule("banner", 3),
-            new AnchorRule("dye", 0)
-    );
 
     private static Map<Item, Set<Item>> buildGraph(List<VanillaRecipeData> vanillaRecipes) {
         Comparator<Item> itemComparator = Comparator.comparing(item ->
@@ -111,38 +59,30 @@ public class TierCalculator {
 
     private static Map<Item, Integer> initializeBaseTiers(Set<Item> allItems, List<VanillaRecipeData> vanillaRecipes, Set<Item> locked)
     {
-        Set<Item> outputs = vanillaRecipes.stream()
-                .map(VanillaRecipeData::output)
-                .collect(Collectors.toSet());
-
+        Set<Item> hasRecipe = vanillaRecipes.stream().map(VanillaRecipeData::output).collect(Collectors.toSet());
         Map<Item, Integer> tiers = new HashMap<>();
 
         for (Item item : allItems) {
 
-            // 1. anchor (жёстко фиксированные тиры)
-            Integer anchor = detectAnchorTier(item);
-            Integer mining = detectMiningTier(item);
+            // Проход по выставленным якорям и проверкам
+            Integer anchorTier = detectAnchorTier(item);
+            Integer miningTier = detectMiningTier(item);
+            Integer rarityTier = detectRarityTier(item);
+            Integer itemTagTier = detectItemTagTier(item);
+            Integer blockTagTier = detectBlockTagTier(item);
 
-            if (anchor != null || mining != null) {
-                int tier = Math.max(
-                        anchor != null ? anchor : 0,
-                        mining != null ? mining : 0
-                );
+            // Находит максимальный из найденных
+            int base = Stream.of(anchorTier, rarityTier, miningTier, itemTagTier, blockTagTier)
+                    .filter(Objects::nonNull)
+                    .max(Integer::compare)
+                    .orElse(-1);
 
-                tiers.put(item, tier);
-                locked.add(item);
-                continue;
+            if (base != -1) {
+                tiers.put(item, base);
+                locked.add(item); // Это основа, она неизменна
+            } else if (hasRecipe.contains(item)) {
+                tiers.put(item, 0);
             }
-
-            // 2. предмет имеет рецепты (может быть скрафчен)
-            if (outputs.contains(item)) {
-                tiers.put(item, -1); // "не вычислен"
-                continue;
-            }
-
-            // 3. остальное — базовый нейтрал
-            tiers.put(item, 0);
-            locked.add(item);
         }
 
         return tiers;
@@ -156,13 +96,10 @@ public class TierCalculator {
             changed = false;
 
             for (Map.Entry<Item, Set<Item>> e : graph.entrySet()) {
-
                 Item out = e.getKey();
-
                 if (locked.contains(out)) continue;
 
                 int current = tiers.getOrDefault(out, -1);
-
                 if (current != -1) continue;
 
                 int maxInput = 0;
@@ -182,7 +119,6 @@ public class TierCalculator {
                 if (!allKnown) continue;
 
                 int candidate = maxInput + 1;
-
                 tiers.put(out, candidate);
                 changed = true;
             }
@@ -191,6 +127,81 @@ public class TierCalculator {
     }
 
     // Правила тиров
+
+    private static final Map<TagKey<Item>, Integer> ITEM_TAG_WEIGHTS = Map.ofEntries(
+            Map.entry(ItemTags.VILLAGER_PLANTABLE_SEEDS, 0),
+            Map.entry(ItemTags.DIRT, 0),
+            Map.entry(ItemTags.SMALL_FLOWERS, 0),
+            Map.entry(ItemTags.BEE_FOOD, 0),
+            Map.entry(ItemTags.BIRCH_LOGS, 0),
+            Map.entry(ItemTags.SAPLINGS, 0),
+            Map.entry(ItemTags.WOOL, 1),
+            Map.entry(ItemTags.PLANKS, 1),
+            Map.entry(ItemTags.LOGS, 2),
+            Map.entry(ItemTags.WOOL_CARPETS, 3),
+            Map.entry(ItemTags.MEAT, 5),
+            Map.entry(ItemTags.FISHES, 5),
+            Map.entry(ItemTags.BEDS, 5),
+            Map.entry(ItemTags.BOATS, 5),
+            Map.entry(ItemTags.COAL_ORES, 6),
+            Map.entry(ItemTags.STONE_TOOL_MATERIALS, 6),
+            Map.entry(ItemTags.COPPER_ORES, 6),
+            Map.entry(ItemTags.STONE_CRAFTING_MATERIALS, 7),
+            Map.entry(ItemTags.STONE_BRICKS, 8),
+            Map.entry(ItemTags.WALLS, 8),
+            Map.entry(ItemTags.IRON_ORES, 9),
+            Map.entry(ItemTags.RAILS, 9),
+            Map.entry(ItemTags.ANVIL, 10),
+            Map.entry(ItemTags.EMERALD_ORES, 10),
+            Map.entry(ItemTags.REDSTONE_ORES, 10),
+            Map.entry(ItemTags.LAPIS_ORES, 10),
+            Map.entry(ItemTags.GOLD_ORES, 10),
+            Map.entry(ItemTags.PIGLIN_FOOD, 11),
+            Map.entry(ItemTags.DIAMOND_ORES, 13),
+            Map.entry(ItemTags.SKULLS, 13),
+            Map.entry(ItemTags.CREEPER_DROP_MUSIC_DISCS, 14),
+            Map.entry(ItemTags.MACE_ENCHANTABLE, 14)
+    );
+
+    private static Integer detectItemTagTier(Item item) {
+        int maxWeight = -1;
+        var holder = BuiltInRegistries.ITEM.wrapAsHolder(item);
+
+        for (var entry : ITEM_TAG_WEIGHTS.entrySet()) {
+            if (holder.is(entry.getKey())) {
+                maxWeight = Math.max(maxWeight, entry.getValue());
+            }
+        }
+        return maxWeight != -1 ? maxWeight : null;
+    }
+
+    private static final Map<TagKey<Block>, Integer> BLOCK_TAG_WEIGHTS = Map.of(
+            BlockTags.REPLACEABLE, 0,
+            BlockTags.BEEHIVES, 8,
+            BlockTags.ICE, 11,
+            BlockTags.NYLIUM, 11,
+            BlockTags.CORAL_BLOCKS, 11,
+            BlockTags.WITHER_SUMMON_BASE_BLOCKS, 12,
+            BlockTags.ANVIL, 12,
+            BlockTags.SHULKER_BOXES, 14,
+            BlockTags.DRAGON_IMMUNE, 14,
+            BlockTags.WITHER_IMMUNE, 14
+    );
+
+    private static Integer detectBlockTagTier(Item item) {
+        if (!(item instanceof BlockItem blockItem)) return null;
+
+        Block block = blockItem.getBlock();
+        var holder = BuiltInRegistries.BLOCK.wrapAsHolder(block);
+        int maxWeight = -1;
+
+        for (var entry : BLOCK_TAG_WEIGHTS.entrySet()) {
+            if (holder.is(entry.getKey())) {
+                maxWeight = Math.max(maxWeight, entry.getValue());
+            }
+        }
+        return maxWeight != -1 ? maxWeight : null;
+    }
 
     private static Integer detectMiningTier(Item item) {
         if (!(item instanceof BlockItem blockItem)) return null;
@@ -211,6 +222,52 @@ public class TierCalculator {
         return null;
     }
 
+    private record AnchorRule(String keyword, int tier) {}
+
+    private static final List<AnchorRule> ANCHOR_RULES = List.of(
+            new AnchorRule("netherite", 15),
+            new AnchorRule("banner_pattern", 15),
+            new AnchorRule("smithing_template", 15),
+
+            new AnchorRule("ender", 14),
+            new AnchorRule("elytra", 14),
+            new AnchorRule("dragon", 14),
+            new AnchorRule("end_", 14),
+            new AnchorRule("chorus", 14),
+            new AnchorRule("shulker", 14),
+            new AnchorRule("purpur", 14),
+
+            new AnchorRule("diamond", 13),
+            new AnchorRule("wither", 13),
+
+            new AnchorRule("blaze", 12),
+            new AnchorRule("ghast", 12),
+            new AnchorRule("magma", 12),
+            new AnchorRule("nether", 12),
+            new AnchorRule("quartz", 12),
+            new AnchorRule("soul", 12),
+            new AnchorRule("crimson", 12),
+            new AnchorRule("warped", 12),
+            new AnchorRule("blackstone", 12),
+            new AnchorRule("basalt", 12),
+
+            new AnchorRule("torchflower", 11),
+            new AnchorRule("sculk", 11),
+            new AnchorRule("pither", 11),
+            new AnchorRule("breeze", 10),
+            new AnchorRule("prismarine", 10),
+            new AnchorRule("gold", 10),
+            new AnchorRule("armadillo_scute", 10),
+            new AnchorRule("iron", 9),
+            new AnchorRule("minecart", 9),
+            new AnchorRule("copper", 7),
+            new AnchorRule("disc", 6),
+            new AnchorRule("spyglass", 6),
+
+            new AnchorRule("banner", 3),
+            new AnchorRule("dye", 1)
+    );
+
     private static Integer detectAnchorTier(Item item) {
         ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
         String path = id.getPath().toLowerCase(Locale.ROOT);
@@ -224,6 +281,20 @@ public class TierCalculator {
         return null;
     }
 
+    private static Integer detectRarityTier(Item item) {
+        Rarity rarity = item.components().getOrDefault(DataComponents.RARITY, Rarity.COMMON);
+
+        return switch (rarity) {
+            case COMMON -> 0;
+            case UNCOMMON -> 7;
+            case RARE -> 10;
+            case EPIC -> 14;
+        };
+    }
+
+    /**
+     * DEBUG-функция для вывода всей таблицы тиров
+     */
     private static void logGroupedTiers(Map<Item, Integer> tiers) {
         Map<Integer, List<Item>> grouped = new TreeMap<>();
 
@@ -235,7 +306,13 @@ public class TierCalculator {
         for (var entry : grouped.entrySet()) {
             LOGGER.info("=== TIER {} ===", entry.getKey());
 
-            for (Item item : entry.getValue()) {
+            List<Item> itemsInTier = entry.getValue();
+
+            itemsInTier.sort(Comparator.comparing(item ->
+                    BuiltInRegistries.ITEM.getKey(item).toString()
+            ));
+
+            for (Item item : itemsInTier) {
                 ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
                 LOGGER.info(" - {}", id.getPath());
             }
